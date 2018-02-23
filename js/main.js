51d5d1d2e94937a9d6571941d0ade93a210fa811
Nestor Devs <nestor@devsstore.net>
2018-02-23 18:58:18 -0400

    fist commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+    var nav = $(".navbar");
+    $(window).scroll(() => {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 300) {
+            nav.addClass("change-color-nav");
+        } else {
+            nav.removeClass("change-color-nav");
+        }
+    });
+    var $btnTop = $(".btn-top");
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() >= 20) {
+            $btnTop.fadeIn();
+        } else {
+            $btnTop.fadeOut();
+        }
+    });
+
+    $btnTop.on("click", function() {
+        $("html, body").animate({ scrollTop: 0 }, 300);
+    });
+});
