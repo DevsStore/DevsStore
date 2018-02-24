e2e54b3a333862918e699f7b6574b9d192f40092
Nestor Devs <nestor@devsstore.net>
2018-02-24 16:56:07 -0400

    borrando css min

--- desarrollo_web.php
+++ desarrollo_web.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.css">
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
