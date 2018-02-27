e42dff5064338ecd80bcb868158f04b1d9bafe2a
Jolver Andre <andre@devsstore.net>
2018-02-27 14:01:37 -0500

    change origin script of page web to main.js

--- js/main.js
+++ js/main.js
@@ -15,6 +15,13 @@ $(document).ready(() => {
         } else {
             $btnTop.fadeOut();
         }
+        $('.demo-card').each( function(i){
+                    var bottom_of_object = $(this).position().top + $(this).outerHeight();
+                    var bottom_of_window = $(window).scrollTop() + $(window).height();
+                    if( bottom_of_window > bottom_of_object ){                
+                        $(this).animate({'opacity':'1'},3000);
+                    } 
+                });
     });
 
     $btnTop.on("click", function() {
