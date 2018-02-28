e484c86cfd8bf941d7c1368ae7e2259fd54b6dad
Jolver Andre <andre@devsstore.net>
2018-02-28 18:36:35 -0500

    cards 2

--- desarrollo_movil.php
+++ desarrollo_movil.php
@@ -47,43 +47,39 @@
 
 </div>
 
-<div class="container" style="margin:2rem;">
+<div class="container" style="margin-top:2rem;margin-bottom:2rem;">
         <div class="row">
-            <div class="col-md-6">
-            <div class="card mi-card">
-                <img class="card-img-top" src="img/seo-px2v2-460x388.png" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card mi-card">
-                        <img class="card-img-top" src="img/seo-px2v2-460x388.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
+            <div class="col-md-4">
+                <div class="card mi-card">
+                    <img class="card-img-top" src="img/seo-px2v2-460x388.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
-                    <div class="col-md-6">
-                    <div class="card mi-card">
-                        <img class="card-img-top" src="img/seo-px2v2-460x388.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mi-card">
+                    <img class="card-img-top" src="img/seo-px2v2-460x388.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mi-card">
+                    <img class="card-img-top" src="img/seo-px2v2-460x388.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 </div>
 <section class="contacto text-center">
 
